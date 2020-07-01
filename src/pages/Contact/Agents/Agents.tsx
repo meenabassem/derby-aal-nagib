a98ae2f73efc9b1b66fb01fcd1604e15c339d5ef
Meena Bassem <mpdotf@gmail.com>
2020-07-01 06:43:49 +0200

    - Added Agents screen

--- src/pages/Contact/Agents/Agents.tsx
+++ src/pages/Contact/Agents/Agents.tsx
@@ -1,6 +1,30 @@
 import React from "react";
+import "App.scss";
+import {Button, Card, Image} from "react-bootstrap";
+import EG_FLAG from "assets/img/flags/eg.jpg";
 
 const Agents = () => {
-  return <div>Agents</div>;
+  return (
+    <div className={"page-body-container"}>
+      <h1>Agents</h1>
+      <p>Contact with our Agents</p>
+      <Card>
+        <Card.Header as="h5">
+          <Image src={EG_FLAG} width={30} />
+          {"  "} Egypt
+        </Card.Header>
+        <Card.Body>
+          <Card.Title>DERBY AAL NAGIB - EGYPT BRANCH</Card.Title>
+          <Card.Text>Contact our branch in Egypt via</Card.Text>
+          <Button variant="primary" href={"mailto:mail@example.com"}>
+            Email
+          </Button>{" "}
+          <Button variant="primary" href={"tel:+201234567890"}>
+            Phone
+          </Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 };
 export { Agents };
--- src/pages/Live/LivePage.tsx
+++ src/pages/Live/LivePage.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import "App.scss";
 
 const LivePage = () => {
+  const containerWidth =
+    (window?.innerWidth > 500 ? 500 : window.innerWidth) - 50;
   return (
     <div className={"page-body-container"}>
       <h3>Live Results</h3>
       <iframe
+        title={"youtube-live"}
         style={{ display: "block", margin: "auto" }}
-        width="560"
+        width={containerWidth}
         height="315"
         src="https://www.youtube-nocookie.com/embed/I7MyWWmRg4A"
         frameBorder="0"
