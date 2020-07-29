3cc41f24ca1274fca7fd5ec7ce147772d56a365b
Meena Bassem <mpdotf@gmail.com>
2020-07-29 08:46:08 +0200

    - Added search in All Events page

--- src/pages/Events/AllEvents.tsx
+++ src/pages/Events/AllEvents.tsx
@@ -1,9 +1,11 @@
 import React, {useEffect, useState} from "react";
 import "App.scss";
 import "./Results.scss";
-import {Button, Table} from "react-bootstrap";
+import {Button, Col, Form, FormControl, InputGroup, Row, Table} from "react-bootstrap";
 import {useHistory} from "react-router-dom";
 import {NetworkHelper} from "modules/network/NetworkHelper";
+import * as yup from "yup";
+import {Formik} from "formik";
 
 //Race & Training results
 
@@ -21,6 +23,7 @@ interface ActionButtonProps {
   buttonType: "Basketing" | "Results";
   eventId: string | number;
 }
+type ApiLoadedStatus = "LOADING" | "LOADED" | "ERROR" | "NO_RESULTS";
 const ActionButton = (props: ActionButtonProps) => {
   const history = useHistory();
   const { eventId, buttonType } = props;
@@ -47,71 +50,145 @@ const ActionButton = (props: ActionButtonProps) => {
 
 const AllEvents = () => {
   const [allEvents, setAllEvents] = useState<EventsAllTableRowEntry[]>([]);
-  useEffect(() => {
-    NetworkHelper.get("/Events")
+
+  const [apiResponseStatus, setApiResponseStatus] = useState<ApiLoadedStatus>(
+    "LOADING"
+  );
+  const loadEventsFromApi = (searchQuery: string = "") => {
+    setApiResponseStatus("LOADING");
+    NetworkHelper.get("/Events", { params: { q: searchQuery } })
       .then(value => {
         if (value?.data && value.data?.length) {
           setAllEvents(value.data);
+          setApiResponseStatus("LOADED");
+        } else {
+          setAllEvents([]);
+          setApiResponseStatus("NO_RESULTS");
         }
       })
       .catch(reason => {
         console.log("Couldn't load Events data");
+        setApiResponseStatus("ERROR");
       });
+  };
+
+  useEffect(() => {
+    loadEventsFromApi();
   }, []);
 
+  const schema = yup.object({
+    searchField: yup.string()
+  });
+
+  const onSearchSubmit = (event: any) => {
+    loadEventsFromApi(event.searchField);
+  };
+  const RenderEventsResults = () => {
+    switch (apiResponseStatus) {
+      case "ERROR":
+        return (
+          <div>
+            An error has occurred while loading the data, please try again later
+          </div>
+        );
+      case "LOADED":
+        return (
+          <Table striped bordered hover size="sm">
+            <thead>
+              <tr>
+                <th>Category</th>
+                <th>Event</th>
+                <th>Date</th>
+                <th>Liberation</th>
+                <th>Distance</th>
+                <th>Status</th>
+                <th></th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {allEvents.map(
+                (resultItem: EventsAllTableRowEntry, resultIndex: number) => {
+                  const {
+                    _id,
+                    Category,
+                    EVENT,
+                    Date,
+                    Liberation,
+                    Status,
+                    Distance
+                  } = resultItem;
+                  return (
+                    <tr key={resultIndex}>
+                      <td>{Category}</td>
+                      <td>{EVENT}</td>
+
+                      <td>{Date}</td>
+                      <td>{Liberation}</td>
+                      <td>{Distance}</td>
+                      <td>{Status}</td>
+                      <td>
+                        <div style={{ display: "flex" }}>
+                          <ActionButton
+                            buttonType={"Basketing"}
+                            eventId={_id}
+                          />
+                          <ActionButton buttonType={"Results"} eventId={_id} />
+                        </div>
+                      </td>
+                    </tr>
+                  );
+                }
+              )}
+            </tbody>
+          </Table>
+        );
+      case "LOADING":
+        return <div>Loading ....</div>;
+      case "NO_RESULTS":
+        return <div>No results found matching your search</div>;
+    }
+  };
   return (
     <div className={"page-body-container results-page"}>
-      <h3>Race & Training Results</h3>
-      {allEvents?.length ? (
-        <Table striped bordered hover size="sm">
-          <thead>
-            <tr>
-              <th>Category</th>
-              <th>Event</th>
-              <th>Date</th>
-              <th>Liberation</th>
-              <th>Distance</th>
-              <th>Status</th>
-              <th></th>
-            </tr>
-          </thead>
-
-          <tbody>
-            {allEvents.map(
-              (resultItem: EventsAllTableRowEntry, resultIndex: number) => {
-                const {
-                  _id,
-                  Category,
-                  EVENT,
-                  Date,
-                  Liberation,
-                  Status,
-                  Distance
-                } = resultItem;
-                return (
-                  <tr key={resultIndex}>
-                    <td>{Category}</td>
-                    <td>{EVENT}</td>
-
-                    <td>{Date}</td>
-                    <td>{Liberation}</td>
-                    <td>{Distance}</td>
-                    <td>{Status}</td>
-                    <td>
-                      <div style={{ display: "flex" }}>
-                        <ActionButton buttonType={"Basketing"} eventId={_id} />
-                        <ActionButton buttonType={"Results"} eventId={_id} />
-                      </div>
-                    </td>
-                  </tr>
-                );
-              }
+      <Row>
+        <Col sm={6}>
+          <h3>Race & Training Results</h3>
+        </Col>
+        <Col sm={6}>
+          <Formik
+            validationSchema={schema}
+            initialValues={{
+              searchField: ""
+            }}
+            onSubmit={onSearchSubmit}
+          >
+            {({ handleSubmit, handleChange, values }) => (
+              <Form noValidate onSubmit={handleSubmit}>
+                <Col>
+                  <Form.Group controlId={"searchField"}>
+                    <InputGroup className="mb-3">
+                      <FormControl
+                        placeholder="Search"
+                        aria-label="Search here"
+                        aria-describedby="basic-addon2"
+                        value={values.searchField}
+                        onChange={handleChange}
+                      />
+                      <InputGroup.Append>
+                        <Button variant="outline-secondary" type={"submit"}>
+                          Search
+                        </Button>
+                      </InputGroup.Append>
+                    </InputGroup>
+                  </Form.Group>
+                </Col>
+              </Form>
             )}
-          </tbody>
-        </Table>
-      ) : (
-        <></>
-      )}
+          </Formik>
+        </Col>
+      </Row>
+      <RenderEventsResults />
     </div>
   );
 };
