4cbf6b572c8408a60b1eb8d57b2a0a3f31d695ca
Meena Bassem <mpdotf@gmail.com>
2020-08-04 21:08:15 +0200

    - Updated terms and conditions

--- src/pages/Terms/TermsAndConditions/TermsAndConditionsPage.tsx
+++ src/pages/Terms/TermsAndConditions/TermsAndConditionsPage.tsx
@@ -4,30 +4,65 @@ import "./TermsAndConditions.scss";
 
 const TermsAndConditionsPage = () => {
   const terms: string[] = [
-    "Participants may enter one or more teams consisting of 1 eligible pigeon and up to 4 reserve pigeons per team (see entry & payment form). An entrant may opt to send fewer or no reserve pigeons in a team. Right of entry is reserved.",
-    "An eligible pigeon is designated as such when the individual bird entry fee of USD 950 (net after bank charges) has been paid and confirmed as being received. All payments must be made to Victoria Falls World Challenge Pigeon Race Ltd (see entry & payment form). An early bird discounted fee of USD 4 250 is offered if an entrant pays for a full team of 5 pigeons. The discounted fee is not for a part team and must be paid before or by 31st December 2019. In which case all 5 pigeons will be activated upon entry and there will be no reserve pigeons for that team.",
-    "If an entrant pays for all their remaining birds, provided it is 10 or more pigeons, by 31 December 2019 then the activation fee will be at USD 850 per bird.",
-    "Should an entrant lose a paid for pigeon and there is no reserve available to replace it prior to entering in the final race and provided that neither it nor one of its subsequent replacements have won any prize money, the entrant will receive a credit in their account of the amount paid for that pigeon to be used towards entries in the following year. The credit under this rule will expire after one race season and is not carried forward thereafter. Any credits for lost pigeons from a previous year will be applied to pigeons in the current year in order of activation, in other words the credits will be used up first. Credits may be used to activate “For Sale” pigeons or reserve pigeons which remain available after Hotspot 5 instead of receiving the credit for use in the following year. The exception to this clause is that any paid for pigeon which is entered into the Super Ace competition cancels out any credits for that activation (see Super Ace terms).",
-    "An entry of at least 1 pigeon per team entered must be paid for prior to the receipt of pigeons by the VFWCPR Ltd designated agents (see designated agents) and proof of payment submitted to the agent and copied to VFWCPR via email (all payments must be net after bank charges). Fancier accounts will be credited with the amount received by VFWCPR. If payment has not been received before or on 31st December 2019 the pigeons may be sold to third parties or removed from the competition.",
-    "Payment for quarantine and shipping must be made by the entrant on admission to the quarantine facility which will be co-ordinated by the designated agent. All pigeons must be vaccinated against Paramyxovirus at 21 to 24 days old, preferably with a herpes PMV combo vaccine. It would also be desirable to vaccinate against paratyphoid.",
-    "There will be several scheduled intakes of pigeons for quarantine prior to shipping which must be organised through designated agents. Intakes by agents will be from July to October 2019 Please contact them for specific dates. VFWCPR reserves the right to cancel later shipments if it is felt that the loft capacity will be exceeded.",
-    "During the period of quarantine and prior to any pigeons being shipped to Zimbabwe an entrant will have the opportunity to send replacement birds should any die or need replacing. Advice of the need to replace must come from your designated VFWCPR agent and onus of liaison about replacements rests with the entrant. No replacements can be shipped from abroad after the last scheduled intake organised by the respective agents. Replacements can be taken from “For Sale” pigeons for a donation of USD 50 which will go to the Wildlife Trust. If taken no guarantee is given on the birds’ performance and survival."
+    "Rings: 2020 rings are requires and we can receive the last season derby's rings, derby’s rings or any external rings for 2020",
+
+    "When delivering birds with the derby’s rings: ",
+    "- Each 5 birds get 1 for free",
+    "- Each 10 birds get 3 for free",
+    "- Each 15 birds get 4 for free",
+    "When delivery birds with external rings: ",
+    "- Each 10 birds get 2 for free",
+    "- Each 15 birds get 3 free ",
+    "- Each 20 birds get 4 for free",
+
+    "Received free birds considered as spare dead or priced birds deducted from it not from paid birds. ",
+    "The pigeons submitted with the participation fortification by changing 2 feathers equal 60 days or more . ",
+    "The price of subscription 87 euro ,58 euro paid with pigeon submit and the rest f price after fixing birds and before releasing birds . ",
+    "In case the birds die after using spare the participation will be restored instead of it next season . ",
+    "After receiving birds in one race point it is not allowed to claim it again The participations not allowed to interfere the management decision. ",
+    "The birds wear the magnetic benzing rings to facilitate the birds periodical inventory by BINZING LIVE . ",
+    "We apologize for not accepting the birds after the birds number is completed . ",
+
+    "Bird’s acceptance starts in 4th of June until 1st of august 2020",
+
+    "Delivery days : ",
+
+    "From 4-6     11-6      18-6    25-6      2-7    9-7      16-7    23-7   30-7",
+    "To       6-6    13-6     20-6     27-6      4-7    11-7    18-7   25-7   1-8",
+
+    "It is allowed to receive any number of birds from the participations . ",
+    "The point responsible for choosing the atmosphere for the race ,the Benzing system opened when the race start and closed next day from the race when the birds come with the sunset . ",
+    "The team contains birds and in case the result are equal the result decided to the highest result of the individual bird . ",
+    "The participation's name  will be able to modified after receiving the birds . ",
+    "The result of Ace pigeon will calculated by the Benzing system . ",
+    "The Ace price for the best birds is provided from the RCM company and it is 3 birds were logged from the first 20 in all stations. ",
+
+    "           Ace:",
+    "The Ace prize for three strongest birds with a position from 1 to 20 in the five races in an amount of 29,500 euro. ",
+
+    "1st      15000 euro",
+    "2nd     8500   euro",
+    "3rd      6000   euro",
+
+    "The point does not have any responsibility for the dead or lost birds until the end of the race and in case of the bird loss or death the contestant is not entitled to claim any of the prepaid fees. ",
+
+    "Official distances: ",
+    "300 km the first race el Dabaa the first week of January ",
+    "350 km the second race Fouka the second week of January ",
+    "450 km the third race marsa matrouh the third week of January ",
+    "500 km semifinal zawyet shemas first week of February ",
+    "620 km final race salloum twelve days after marsa matruh race "
   ];
   return (
     <div className={"terms-and-conditions-top-container page-body-container"}>
-      <h3>Terms and conditions VFWCPR – 2019 entrants for 2020 race series</h3>
+      <h3>Terms & Conditions (2020) :</h3>
       <ul>
         {terms.map((term, termIndex) => (
           <li key={termIndex}>{term}</li>
         ))}
       </ul>
       <div className={"signature"}>
-        <p>Managing Director,</p>
-        <p>Victoria Falls World Challenge Pigeon Race Ltd,</p>
-        <p>Montagu Pavilion,</p>
-        <p>8-10 Queensway,</p>
-        <p>GX 111 AA,</p>
-        <p>Gibraltar.</p>
+        <p>Managing Director</p>
       </div>
     </div>
   );
