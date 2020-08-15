3d4e779c9c1aa20680bbd638fcfc4ffbe2b4d785
Meena Bassem <mpdotf@gmail.com>
2020-08-16 01:39:13 +0200

    - Added dark header/footer for mobile app - Added arabic entries in prize money

--- src/components/Navbar/AppNavBar.tsx
+++ src/components/Navbar/AppNavBar.tsx
@@ -1,8 +1,9 @@
 import React, {useEffect, useState} from "react";
 import {Nav, Navbar, NavDropdown} from "react-bootstrap";
-import Logo from "assets/img/logo-transparent.png";
+import Logo from "assets/img/logo-transparent-black-text.png";
 
 import moment from "moment";
+import {Capacitor} from "@capacitor/core";
 
 const AppNavBar = () => {
   const [currentTime, setCurrentTime] = useState<string>();
@@ -11,12 +12,13 @@ const AppNavBar = () => {
       setCurrentTime(moment().format("MMMM Do YYYY, h:mm:ss A"));
     }, 1000);
   }, []);
+  const colorVariant = Capacitor.isNative ? "dark" : "light";
   return (
     <Navbar
       collapseOnSelect
       expand="lg"
-      bg="light"
-      variant="light"
+      bg={colorVariant}
+      variant={colorVariant}
       sticky="top"
       style={{ boxShadow: "0 0px 5px #888888" }}
     >
@@ -50,34 +52,50 @@ const AppNavBar = () => {
             </NavDropdown.Item>
             <NavDropdown.Item href="/gallery/videos">Videos</NavDropdown.Item>
           </NavDropdown>
-          <NavDropdown title="Terms" id="basic-nav-dropdown">
-            <NavDropdown.Item href="/terms/terms-and-conditions">
-              Terms & Conditions
-            </NavDropdown.Item>
-            <NavDropdown.Item href="/terms/prize ">
-              Prize Money
-            </NavDropdown.Item>
 
-            <NavDropdown.Item href="/terms/forms">Forms</NavDropdown.Item>
-          </NavDropdown>
+          {!Capacitor.isNative ? (
+            <NavDropdown title="Terms" id="basic-nav-dropdown">
+              <NavDropdown.Item href="/terms/terms-and-conditions">
+                Terms & Conditions
+              </NavDropdown.Item>
+              <NavDropdown.Item href="/terms/prize ">
+                Prize Money
+              </NavDropdown.Item>
+
+              <NavDropdown.Item href="/terms/forms">Forms</NavDropdown.Item>
+            </NavDropdown>
+          ) : (
+            <></>
+          )}
+
           <NavDropdown title="Contact" id="basic-nav-dropdown">
             <NavDropdown.Item href="/contact/agents">Agents</NavDropdown.Item>
-            <NavDropdown.Item href="/contact/enquiries">
-              Enquiries
-            </NavDropdown.Item>
+
+            {!Capacitor.isNative ? (
+              <NavDropdown.Item href="/contact/enquiries">
+                Enquiries
+              </NavDropdown.Item>
+            ) : (
+              <></>
+            )}
           </NavDropdown>
-          <NavDropdown title="Login" id="basic-nav-dropdown">
-            <NavDropdown.Item href="/login/agent">Agent Login</NavDropdown.Item>
-            <NavDropdown.Item href="/login/fancier">
-              Fancier Login
-            </NavDropdown.Item>
-          </NavDropdown>
+          {!Capacitor.isNative ? (
+            <NavDropdown title="Login" id="basic-nav-dropdown">
+              <NavDropdown.Item href="/login/agent">
+                Agent Login
+              </NavDropdown.Item>
+              <NavDropdown.Item href="/login/fancier">
+                Fancier Login
+              </NavDropdown.Item>
+            </NavDropdown>
+          ) : (
+            <></>
+          )}
           {/*<Nav.Link href="/live" style={{ backgroundColor: "red" }}>*/}
           {/*  Live*/}
           {/*</Nav.Link>*/}
 
           {/*<Nav.Link href="#pricing">Pricing</Nav.Link>*/}
-
         </Nav>
         <Nav.Item style={{}} className={"nav-link"}>
           {currentTime}
