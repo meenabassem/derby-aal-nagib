81d64b15bac56c972e5b1df1a745ce1e52b68231
root <root@srv.derbyaalnagib.com>
2020-08-16 01:07:49 +0200

    Set network path for production

--- src/modules/network/NetworkHelper.ts
+++ src/modules/network/NetworkHelper.ts
@@ -3,7 +3,8 @@ import Axios, {AxiosInstance} from "axios";
 const NetworkHelper: AxiosInstance = Axios.create({
   // baseURL: "https://derbyaalnagib.herokuapp.com/"
   // baseURL: "http://192.168.1.15:3001/api"
-  baseURL: "https://derbyaalnagib.com/api"
+//  baseURL: "https://derbyaalnagib.com/api"
+baseURL:"/api"
 });
 
 export { NetworkHelper };
