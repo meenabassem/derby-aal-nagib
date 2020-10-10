64581ace55a643e78f19cfbe7ff085dd3f4b9f68
Meena Bassem <mpdotf@gmail.com>
2020-10-10 05:21:01 +0200

    - Fixed API URL

--- src/modules/network/NetworkHelper.ts
+++ src/modules/network/NetworkHelper.ts
@@ -3,8 +3,8 @@ import Axios, {AxiosInstance} from "axios";
 // baseURL: "https://derbyaalnagib.herokuapp.com/"
 // baseURL: "http://192.168.1.15:3001/api"
 // baseURL: "https://derbyaalnagib.com/api"
-const baseURL:string = "http://localhost:3001/api"
-// const baseURL:string = "/api"
+// const baseURL:string = "http://localhost:3001/api"
+const baseURL:string = "/api"
 
 const NetworkHelper: AxiosInstance = Axios.create({
   baseURL
